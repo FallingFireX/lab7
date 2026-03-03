9086d7ea839c9bde90405a737b3297835e833788
FallingFireX <evesperson@gmail.com>
2026-03-03 16:40:34 -0700

    Update half_sub.v

---
+++ half_sub.v
@@ -0,0 +1,10 @@
+module half_sub(
+    input A, B, // Declare your A/B inputs
+    output Y, // Declare Y output
+    output carry// Declare carry output
+);
+
+    assign Y = A ^ B 
+    assign carry = A & B 
+        
+endmodule

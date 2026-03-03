b0dda8260db5385254a3ca94b09f0bec1e2fb325
FallingFireX <evesperson@gmail.com>
2026-03-03 16:44:33 -0700

    Update top.v

---
+++ top.v
@@ -0,0 +1,31 @@
+module top(
+  input [7:0] sw,
+  output [5:0] led
+  
+);
+
+  half_sub sub_inst(
+    .A(sw[0]),
+    .B(sw[1]),
+    .Y(led[0]),
+    .borrow(led[0])
+    );
+  
+  
+  wire carry_between
+  full_adder FA1 (
+      .A(sw[4]),
+      .B(sw[5]),
+    .Cin(1'b0)
+      .Y(led[3]),
+      .carry(carry_between)
+    );
+  full_adder FA2 (
+    .A(sw[6]),
+    .B(sw[7]),
+    .Cin(carry_between)
+    .Y(led[4]),
+    .carry(led[5])
+  );
+
+endmodule

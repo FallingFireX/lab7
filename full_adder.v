51c1dda33a0406bdb2db9ade358ba5bd0aa4180a
collingreen-del <collingreen@u.boisestate.edu>
2026-03-03 17:01:12 -0700

    Implement full adder module with logic equations

--- full_adder.v
+++ full_adder.v
@@ -1,1 +1,10 @@
+module full_adder(
+  input A, B, Cin,
+  output Y,
+  output carry
+);
+  
+  assign Y = ( ~A & ~B & Cin) | ( ~A & B & ~ Cin) | (A & B & Cin) | (A & ~B & ~Cin) ;//equation here
+  assign carry = (A & Cin) | (B & Cin) | (A & B);//equation here
 
+endmodule

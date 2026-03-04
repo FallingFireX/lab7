8acfbe030ee97eb51426f97d8c8ceca0be811f44
collingreen-del <collingreen@u.boisestate.edu>
2026-03-03 17:32:01 -0700

    Refactor full adder connections and outputs

--- ones_compliment.v
+++ ones_compliment.v
@@ -2,12 +2,13 @@ input A,B
 
 wire carry1, carry2, carry3, carry4, carry5, carry6, carry7
 input a,b,C
+wire [3:0] sum
 output Y,
 full_adder FA1 (
   .A(sw[2]),
   .B(sw[6]),
   .Cin(1'b0)
-  .Y(led[2]),
+  .Y(sum[0]),
   .carry(carry1)
     );
 
@@ -15,7 +16,7 @@ full_adder FA1 (
     .A(sw[3]),
     .B(sw[7]),
     .Cin(carry1)
-    .Y(led[3]),
+    .Y(sum[1]),
     .carry(carry2)
   );
     
@@ -23,44 +24,54 @@ full_adder FA1 (
       .A(sw[4]),
       .B(sw[8]),
      .Cin(carry2)
-      .Y(led[4]),
+     .Y(sum[2]),
      .carry(carry3)
   );
 
   full_adder FA4 (
-    .A(sw[5]
+    .A(sw[5])
     .B(sw[9]),
     .Cin(carry3)
-    .Y(led[5])
+       .Y(sum[3])
     .carry(carry4),
    );
     full_adder FA5 (
-    .A(sw[3]),
+      .A(sum[0]),
     .B(1'b0),
       .Cin(carry4)
-    .Y(led[3]),
+      .Y(led[2]),
       .carry(carry5)
   );
     
    full_adder FA6 (
-      .A(sw[4]),
+     .A(sum[1]),
       .B(1'b0),
      .Cin(carry5)
-      .Y(led[4]),
+     .Y(led[3]),
      .carry(carry6)
   );
 
   full_adder FA7 (
-    .A(sw[5]
+    .A(sum[2])
     .B(1'b0),
        .Cin(carry6)
-    .Y(led[5])
+       .Y(led[4])
        .carry(carry7),
+      ); 
+    
+    full_adder FA8 (
+    .A(sum[3])
+    .B(1'b0),
+    .Cin(carry7)
+    .Y(led[5])
+       .carry(),
    );
 
+
     
 
     
+
 
 
 

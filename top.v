2d005d9c62b8fd535bd999cf1ad4bb74e8e2a794
collingreen-del <collingreen@u.boisestate.edu>
2026-03-03 17:45:04 -0700

    Refactor top module and remove full adder instances

--- top.v
+++ top.v
@@ -12,21 +12,8 @@ module top(
     );
   
   
-  wire carry_between
-  full_adder FA1 (
-      .A(sw[4]),
-      .B(sw[5]),
-    .Cin(1'b0)
-      .Y(led[3]),
-      .carry(carry_between)
-    );
-  full_adder FA2 (
-    .A(sw[6]),
-    .B(sw[7]),
-    .Cin(carry_between)
-    .Y(led[4]),
-    .carry(led[5])
-  );
+
 
 endmodule
+
 

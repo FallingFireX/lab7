46eddae160944fa690e21d59f7cde25764bc89fc
collingreen-del <collingreen@u.boisestate.edu>
2026-03-03 17:15:45 -0700

    Update full adder connections and inputs
    Refactor full adder instances and inputs for carry handling.

--- ones_compliment.v
+++ ones_compliment.v
@@ -1,6 +1,39 @@
 input A,B
-input [9:2] sw
-output [5:2] led
 
-output Compliment
+wire carry_between
+input a,b,C
+output Y, Carry
+full_adder FA1 (
+  .A(sw[2]),
+  .B(sw[6]),
+  .Cin(1'b0)
+  .Y(led[2]),
+  .carry(carry_between)
+    );
+
+  full_adder FA2 (
+    .A(sw[3]),
+    .B(sw[7]),
+    .Cin(carry_between)
+    .Y(led[3]),
+    .carry(carry_between)
+  );
+    
+   full_adder FA3 (
+      .A(sw[4]),
+      .B(sw[8]),
+      .Cin(carry_between)
+      .Y(led[4]),
+      .carry(carry_between)
+  );
+
+  full_adder FA4 (
+    .A(sw[5]
+    .B(sw[9]),
+    .Cin(carry_between)
+    .Y(led[5]),
+   );
+
+    
+
 
